9c0f732d5ebc05019ba07d6b4fb760e787e0d6a4
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-06-08 15:30:22 -0400

    completed

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,26 +1,33 @@
 import Card from 'react-bootstrap/Card';
 import banner from './banner.webp';
-
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Banner() {
   return (
-
-    <Card style={{ width: '13rem'}} className="bg-dark text-black">
-      <Card.Img variant="right" src={banner} background-color="#deedd6" alt="Card image" />
+<Container style={{ backgroundColor: '#deedd6'}}>
+    <Row><Col><p></p><h1>Order groceries for delivery or pickup today</h1>
+    <p>Wherever you want from local stores, brought to your door.</p>
+    <p>   Enter your address</p>
+    </Col>
+    <Card style={{ width: '13rem'}} className="bg-white text-black">
+      <Card.Img src={banner} alt="Card Image" />
       <Card.ImgOverlay>
         <Card.Title>
-            Order groceries for delivery or pickup today
+            
             </Card.Title>
         <Card.Text>
-            Wherever you want from local stores, brought to your door.
+            
         </Card.Text>
         <Card.Text>
-            Enter your address
+         
         </Card.Text>
       </Card.ImgOverlay>
     </Card>
-    
+
+    </Row>
+</Container>
   );
 }
 
