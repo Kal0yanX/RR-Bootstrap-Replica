1a19759ae324464c6780467a8022910b3c169620
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-06-08 15:49:43 -0400

    looks beautiful

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -7,21 +7,21 @@ import Col from 'react-bootstrap/Col';
 function Banner() {
   return (
 <Container style={{ backgroundColor: '#deedd6'}}>
-    <Row><Col><p></p><h1>Order groceries for delivery or pickup today</h1>
+    <Row style={{ padding: 10}}><Col><p></p><h1>Order groceries for delivery or pickup today</h1>
     <p>Wherever you want from local stores, brought to your door.</p>
     <p>   Enter your address</p>
     </Col>
-    <Card style={{ width: '13rem'}} className="bg-white text-black">
+    <Card style={{ width: '13rem', margin: 0, padding: 0 }} className="text-black">
       <Card.Img src={banner} alt="Card Image" />
-      <Card.ImgOverlay>
+      <Card.ImgOverlay >
         <Card.Title>
-            
+            {/* <h1>Old use of coding with text over banner</h1> */}
             </Card.Title>
         <Card.Text>
-            
+            {/* <h1>Old use of coding with text over banner</h1> */}
         </Card.Text>
         <Card.Text>
-         
+            {/* <h1>Old use of coding with text over banner</h1> */}
         </Card.Text>
       </Card.ImgOverlay>
     </Card>
